4bc803b8763436fcd8c4471a78be422aaa232a41
Neil Humphrey <neilhumphreydesign@gmail.com>
2025-08-23 10:27:43 -0500

    dsds

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://Mediviz-portfolio-v2.vercel.app/sitemap.xml",
+  sitemap: "https://vismedstudio.vercel.app/sitemap.xml",
   }
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,25 +3,25 @@ import type { MetadataRoute } from "next"
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://Mediviz-portfolio-v2.vercel.app",
+      url: "https://vismedstudio.vercel.app",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: "https://Mediviz-portfolio-v2.vercel.app/#portfolio",
+      url: "https://vismedstudio.vercel.app/#portfolio",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://Mediviz-portfolio-v2.vercel.app/#about",
+      url: "https://vismedstudio.vercel.app/#about",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.6,
     },
     {
-      url: "https://Mediviz-portfolio-v2.vercel.app/#contact",
+      url: "https://vismedstudio.vercel.app/#contact",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.7,
